87bbd7365a3b2eee15bc801198e9e86f9d6e16fd
Leegunmyeong <108195045+Leegunmyeong@users.noreply.github.com>
2025-11-22 23:27:01 +0900

    app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, render_template
 import speedtest
+import os
 
 app = Flask(__name__)
 
@@ -25,6 +26,8 @@ def speedtest_api():
     return jsonify(result)
 
 if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
+
 
 
